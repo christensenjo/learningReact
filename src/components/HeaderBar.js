687ce7535c3a9d6e894e155b914cb6a33fc67346
Joel Christensen <christensenjo14@gmail.com>
2023-01-17 14:29:31 -0700

    Continued header improvements

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import sig from './signature_once.gif';
 import linkedin from './linkedin_logo.png';
+import github from './github_logo.png';
 import './HeaderBar.css';
 
 function HeaderBar(props){
@@ -8,7 +9,11 @@ function HeaderBar(props){
         <div>
             <header className="HeaderBar">
                 <img src={sig} className="Signature-gif" alt="signature" />
-                <nav>
+                <nav className="Nav-pages">
+                    <a href="https://learningreact-cc1a4.web.app/" className="Nav-link" alt="Link to home">Home</a>
+                </nav>
+                <nav className="Nav-socials">
+                <a href="https://github.com/christensenjo"><img src={github} className="LinkLogo" alt="Github Logo" /></a>
                     <a href="https://www.linkedin.com/in/joel-christensen-usu/"><img src={linkedin} className="LinkLogo" alt="LinkedIn Logo" /></a>
                 </nav>
             </header>
